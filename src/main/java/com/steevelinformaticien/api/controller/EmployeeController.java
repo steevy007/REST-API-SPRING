bb6944bea71c56092b37f5b43f4cecf142180ae0
steevy <steevesanon6@gmail.com>
2023-02-21 13:28:02 -0500

    push base API code

---
+++ src/main/java/com/steevelinformaticien/api/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.steevelinformaticien.api.controller;
+
+import com.steevelinformaticien.api.model.Employee;
+import com.steevelinformaticien.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+}
---
+++ src/main/java/com/steevelinformaticien/api/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.steevelinformaticien.api.service;
+import java.util.Optional;
+
+import com.steevelinformaticien.api.model.Employee;
+import com.steevelinformaticien.api.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+
+@Data
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Optional<Employee> getEmployee(final Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(final Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return savedEmployee;
+    }
+
+}
